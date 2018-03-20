b975b8057e423e3feee7e49b9012fad35b9da897
huynv05 <37153507+huynv05@users.noreply.github.com>
2018-03-20 11:56:10 +0700

    Add files via upload

--- main.bundle.js
+++ main.bundle.js
@@ -60,6 +60,41 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 
 
 
@@ -104,6 +139,7 @@ var AppComponent = /** @class */ (function () {
             _this.waiting().then(function (res) {
                 _this.audio.pause();
                 if (res === 1) {
+                    _this.Oncalled = false;
                     var videor = _this.myVideo.nativeElement;
                     openStream().then(function (stream) {
                         // var track = stream.getTracks();  // if only one media track
@@ -111,7 +147,6 @@ var AppComponent = /** @class */ (function () {
                         // track[i].stop();
                         var videor = _this.myVideo.nativeElement;
                         _this.streams.push(stream);
-                        _this.Oncalled = false;
                         call.answer(stream);
                         videor.src = URL.createObjectURL(stream);
                         _this.startCall = false;
@@ -140,9 +175,9 @@ var AppComponent = /** @class */ (function () {
                 resolve(1);
             }
             else {
-                setTimeout(function () {
+                return setTimeout(function () {
                     resolve(_this.getPeerID());
-                }, 1000);
+                }, 500);
             }
         });
     };
@@ -201,22 +236,45 @@ var AppComponent = /** @class */ (function () {
         //   conn.send('Message from that id');
         // });
         var video = this.reVideo.nativeElement;
-        openStream().then(function (stream) {
-            console.log(stream);
-            _this.streams.push(stream);
-            video.src = URL.createObjectURL(stream);
-            video.play();
-            _this.Oncalled = true;
-            // this.peer.send(this.user);
-            var call = _this.peer.call(_this.anotherid, stream);
-            call.on("stream", function (remoteStream) {
-                _this.streams.push(remoteStream);
-                _this.startTimeCall = true;
-                var videor = _this.myVideo.nativeElement;
-                console.log(JSON.stringify(remoteStream));
-                videor.src = URL.createObjectURL(remoteStream);
-                videor.play();
+        openStream().then(function (stream) { return __awaiter(_this, void 0, void 0, function () {
+            var _this = this;
+            return __generator(this, function (_a) {
+                console.log(stream);
+                this.streams.push(stream);
+                video.src = URL.createObjectURL(stream);
+                video.play();
+                this.Oncalled = true;
+                // this.peer.send(this.user);
+                // var call= this.peer.call(this.anotherid,stream);
+                // console.log(this.anotherid)
+                // console.log(call)
+                this.calling(stream).then(function (call) {
+                    _this.Oncalled = false;
+                    _this.startTimeCall = true;
+                    call.on("stream", function (remoteStream) {
+                        _this.streams.push(remoteStream);
+                        var videor = _this.myVideo.nativeElement;
+                        console.log(JSON.stringify(remoteStream));
+                        videor.src = URL.createObjectURL(remoteStream);
+                        videor.play();
+                    });
+                });
+                return [2 /*return*/];
             });
+        }); });
+    };
+    AppComponent.prototype.calling = function (stream) {
+        var _this = this;
+        var call = this.peer.call(this.anotherid, stream);
+        return new Promise(function (resolve, reject) {
+            if (call) {
+                resolve(call);
+            }
+            else {
+                setTimeout(function () {
+                    resolve(_this.calling(stream));
+                }, 300);
+            }
         });
     };
     AppComponent.prototype.waiting = function () {
